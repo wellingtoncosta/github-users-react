8aeccafdb626c05e8e1c633ee6882472faccbbda
Wellington Costa <wellington.costa128@gmail.com>
2018-07-02 15:44:29 -0300

    WIP: Rewriting UI components

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Toolbar extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Toolbar </h2>
+      </div>
+    );
+  }
+}
+
+export default Toolbar;
---
+++ src/components/user/details/UserDetails.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserDetails extends Component {
+  render() {
+    return (
+      <div>
+        <h2> User Details </h2>
+      </div>
+    );
+  }
+}
+
+export default UserDetails;
