a5797842b0588f1ad1f83185a66bf0e5a9de5891
Wellington Costa <wellington.costa128@gmail.com>
2018-07-02 09:53:21 -0300

    Updating deps.

--- src/components/userinfo/UserInfo.jsx
+++ src/components/userinfo/UserInfo.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import UserNotFound from './UserNotFound.jsx';
 import UserProfile from './UserProfile.jsx';
 import UserRepositories from './UserRepositories';
